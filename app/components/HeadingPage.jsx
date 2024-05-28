f298cafe6243a778e810fcbd80c9398084a75b59
hamdink <hamdi.zraier@dundill.com>
2024-05-28 21:03:35 +0100

    swiper fixs

--- app/components/HeadingPage.jsx
+++ app/components/HeadingPage.jsx
@@ -12,7 +12,7 @@ const HeadingPage = ({
   p_heading2_1,
   p_heading2_2,
 }) => (
-  <section className="h-[150vh] bg-[url('/assets/background/heading-bg.png')] bg-top bg-cover bg-no-repeat lg:mb-24 md:mb-6 mb-48">
+  <section className="min-h-screen bg-[url('/assets/background/heading-bg.png')] bg-top bg-cover bg-no-repeat lg:mb-24 md:mb-6 mb-48">
     <div className="w-full flex flex-col items-center justify-center mt-14">
       <h1 className="headingPage">{heading1}</h1>
       <p className="p_heading">
@@ -20,7 +20,7 @@ const HeadingPage = ({
         <br />
         {p_heading1_2}
       </p>
-      <div className="flex flex-col md:flex-row gap-2 sm:gap-3 items-center lg:px-0 px-4 mt-4">
+      <div className="flex flex-col lg:flex-row gap-2 sm:gap-3 items-center lg:px-0 px-4 mt-4">
         <Image src={image1_src} alt={image1_alt} width={595} height={393} />
         <Image src={image2_src} alt={image2_alt} width={320} height={393} />
       </div>
--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -8,8 +8,8 @@ import "swiper/css/pagination";
 
 const Testimonials = () => {
   return (
-    <section className="w-full py-10 px-40 lg:h-[500px]">
-      <div className="w-full flex lg:flex-row flex-col items-center pt-4 lg:h-full">
+    <section className="w-full py-10 px-6 lg:px-40 lg:h-[500px] overflow-hidden">
+      <div className="w-full flex xl:flex-row flex-col items-center pt-4 lg:h-full gap-y-[61px] overflow-hidden h-full">
         <div className="flex flex-col items-start flex-[1] sm:pl-0 pl-6 lg:h-[90%]">
           <h3 className="font-poppins text-[#D7AB32] sm:text-[19px] text-[16px] font-medium sm:leading-[29px] leading-[24px]">
             Testimonials
@@ -22,26 +22,29 @@ const Testimonials = () => {
           <p className="font-poppins text-[#2B2B2B] sm:text-[16px] text-[13px] font-normal sm:leading-[26px] leading-[22px] sm:w-[415px] w-[333px] sm:mt-4 mt-2">
             Hear from Satisfied Clients About Their SAK Real Estate Experience
           </p>
-          <div className="flex gap-x-8 sm:mt-8 mt-4">
-            <Image
-              src="/assets/arrowLeft.png"
-              alt="arrowLeft"
-              width={50}
-              height={50}
-              className="sm:block hidden"
-            />
-            <Image
-              src="/assets/arrow.png"
-              alt="arrowRight"
-              width={50}
-              height={50}
-              className="sm:block hidden"
-            />
-          </div>
+            {/* <div className="flex gap-x-8 sm:mt-8 mt-4">
+              <Image
+                src="/assets/arrowLeft.png"
+                alt="arrowLeft"
+                width={50}
+                height={50}
+                className="sm:block hidden"
+              />
+              <Image
+                src="/assets/arrow.png"
+                alt="arrowRight"
+                width={50}
+                height={50}
+                className="sm:block hidden"
+              />
+            </div> */}
         </div>
-        <div className="w-full flex-[1] lg:h-[90%]">
-          <Swiper direction={"vertical"}>
-            <SwiperSlide>
+        <div className="w-full lg:flex-[1] h-[450px]  overflow-hidden mt-20 sm:mt-0">
+          <Swiper direction={"vertical"} className="w-[350px] sm:w-[450px] lg:w-auto" style={{height:"100%",overflowY:'hidden'}}>
+            <SwiperSlide >
+              <TestimonialCard />
+            </SwiperSlide>
+            <SwiperSlide >
               <TestimonialCard />
             </SwiperSlide>
           </Swiper>
